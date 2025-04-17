9512672039fc2cce31b718e24e7a4af8d7ea97ec
hibby <hibaqku7@gmail.com>
2025-04-17 11:09:35 +0300

    Added onclick button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import { Outlet } from "react-router-dom";
+import {useState, useEffect} from "react"
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLogggedIn] = useState=(false);
+  const navigate= useNavigate();
 
+  const logIn =() => {
+    setIsLogggedIn(true);
+  }
+
+const logout = () => {
+  setIsLogggedIn(false);
+};
+useEffect(() =>{
+  if (isLoggedIn) {
+    navigate("/");
+  }
+  else{
+    navigate("/login");
+  };
+}, {isLoggedIn})
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      <NavBar  logout={logout}/>
+      <Outlet context={logIn}/>
     </div>
   );
 }
